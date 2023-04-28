74eb34bb012d14410fd13594174e6b49ea7c5aa6
JoshuaRamc <joshuaramcharan818@gmail.com>
2023-04-27 21:45:50 -0400

    temp commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import NavBar from "./components/NavBar.js";
+import {Button} from "@mui/material"
 
 function App() {
   return (
     <>
       <NavBar />
+
+      <Button>API CALL</Button>
     </>
   );
 }
