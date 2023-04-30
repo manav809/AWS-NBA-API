23d461f26da897a6b496106df4bd9f8d181e467e
manav809 <manavpatel6978@gmail.com>
2023-04-30 12:12:30 -0400

    API Built

---
+++ backend/dynamo.js
@@ -0,0 +1,36 @@
+const AWS = require("aws-sdk");
+require("dotenv").config();
+
+AWS.config.update({
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "playerstatsdata";
+
+async function getPlayers() {
+  const params = {
+    TableName: TABLE_NAME,
+  };
+  const players = await dynamoClient.scan(params).promise();
+  return players;
+}
+
+async function getPlayerById(id) {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id,
+    },
+  };
+  const player = await dynamoClient.get(params).promise();
+  return player;
+}
+
+module.exports = {
+  dynamoClient,
+  getPlayerById,
+  getPlayers,
+};
---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { getPlayers, getPlayerById } = require("./dynamo");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Entry");
+});
+
+app.get("/players", async (req, res) => {
+  try {
+    const players = await getPlayers();
+    res.json(players);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Server Problem" });
+  }
+});
+
+app.get("/players/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const players = await getPlayerById(id);
+    res.json(players);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Server" });
+  }
+});
+
+app.listen(port, () => {
+  console.log("listening on port 3000");
+});
