3d50fb95f5a8f76092d05c4bf7907987694ef444
manav809 <manavpatel6978@gmail.com>
2023-05-01 10:32:48 -0400

    API Completed

--- backend/dynamo.js
+++ backend/dynamo.js
@@ -8,7 +8,7 @@ AWS.config.update({
 });
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
-const TABLE_NAME = "playerstatsdata";
+const TABLE_NAME = "nbaplayerstats";
 
 async function getPlayers() {
   const params = {
