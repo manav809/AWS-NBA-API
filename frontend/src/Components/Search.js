0dcd5a6daaad3b78a0bdf22f1a04db888ae3199c
manav809 <manavpatel6978@gmail.com>
2023-05-01 22:41:24 -0400

    API Call Field From Previous Frontend

--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -1,8 +1,49 @@
 import "../App.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import IconButton from "@mui/material/IconButton";
+const classes = {
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "2rem",
+  },
+};
+function FullWidthTextField() {
+  return (
+    <div style={classes.container}>
+      <div>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+          }}
+        >
+          <TextField
+            fullWidth
+            label="API Call"
+            id="fullWidth"
+            defaultValue="http://ourapi.com/player/"
+          />
+        </Box>
+      </div>
+      <div>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
 function Search() {
   return (
     <div className="App">
-      <h3 className="App-header">Search Player</h3>
+      <h3 className="App-header">
+        <div>Search Player</div>
+        <FullWidthTextField />
+      </h3>
     </div>
   );
 }
