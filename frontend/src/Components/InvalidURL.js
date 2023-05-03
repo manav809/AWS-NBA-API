5adcd53a76ffac37fd8a4e0a10a3ac470686800b
AkshaPatel <akshpatel564@gmail.com>
2023-05-03 19:59:55 -0400

    navbar styling and invalid URL handling

---
+++ frontend/src/Components/InvalidURL.js
@@ -0,0 +1,13 @@
+import "../App.css";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+function InvalidURL() {
+  return (
+    <div className="App">
+      <h3 className="App-header">Error 404</h3>
+    </div>
+  );
+}
+
+export default InvalidURL;
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -7,6 +7,7 @@ import Button from "@mui/material/Button";
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import HomeIcon from '@mui/icons-material/Home';
 import ArticleIcon from '@mui/icons-material/Article';
+import GitHubIcon from '@mui/icons-material/GitHub';
 function Navbar() {
   const navRef = useRef();
 
@@ -16,30 +17,39 @@ function Navbar() {
 
   return (
     <header>
-      <SportsBasketballIcon />
-      <nav ref={navRef}>
-        <Link to="/">
-          <Button variant="text" sx={{ color: "white" }} startIcon={<HomeIcon />}>
-            Home
-          </Button>
-        </Link>
-        <Link to="/Search">
-          <Button variant="text" sx={{ color: "white" }} startIcon={<PersonSearchIcon />}>
-            Search
-          </Button>
-        </Link>
-        <Link to="/Documentation">
-          <Button variant="text" sx={{ color: "white" }} startIcon={<ArticleIcon />}>
-            Documentation
-          </Button>
-        </Link>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
+      <div>
+        <SportsBasketballIcon fontSize="large"></SportsBasketballIcon>
+      </div>
+      <div>
+        <nav ref={navRef}>
+          <Link to="/">
+            <Button variant="text" sx={{ color: "white" }} startIcon={<HomeIcon />}>
+              Home
+            </Button>
+          </Link>
+          <Link to="/Search">
+            <Button variant="text" sx={{ color: "white" }} startIcon={<PersonSearchIcon />}>
+              Search
+            </Button>
+          </Link>
+          <Link to="/Documentation">
+            <Button variant="text" sx={{ color: "white" }} startIcon={<ArticleIcon />}>
+              Documentation
+            </Button>
+          </Link>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
         </button>
-      </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-        <FaBars />
-      </button>
+      </div>
+      <div>
+        <Link to="https://github.com/manav809/AWS-NBA-API" target="_blank">
+          <GitHubIcon fontSize="large" style={{color: "white"}}></GitHubIcon>
+        </Link>
+      </div>
     </header>
   );
 }
