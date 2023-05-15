5eac0259742628d5f2ae438706871665e6239969
manav809 <manavpatel6978@gmail.com>
2023-05-15 16:16:37 -0400

    IFrame Fix

--- frontend/src/Components/Iframes.js
+++ frontend/src/Components/Iframes.js
@@ -37,42 +37,22 @@ export default function Iframes() {
         </Grid>
         <Grid item sm={12} md={6} style={classes.container}>
           <iframe
-            title="Playoff 1"
+            title="ECF"
             frameborder="0"
             scrolling="no"
             width="500"
             height="230"
-            src="https://statsdream.com/basketball/teams/philadelphia-76ers-368043/iframe/?type=team-next-match&sport=4&country=231&template=824&conference=Western Conference&team=368043&timezone=Pacific/Midway&time=24&width=500&height=230&font=Arial&fs=12&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&scfs=22&scfc=333333&scb=1&sclg=1&teamls=80&sh=1&hfb=1&hbc=ECC9EE&hfc=FFFFFF"
+            src="https://statsdream.com/basketball/teams/miami-heat-367742/iframe/?type=team-next-match&sport=4&country=231&template=824&conference=Eastern Conference&team=367742&timezone=Pacific/Midway&time=24&po=1&ma=0&wi=1&los=1&pts=0&ng=0&form=0&width=500&height=700&font=Arial&fs=12&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&ths=1&thb=1&thba=FFFFFF&thc=000000&bc=dddddd&hob=f5f5f5&hobc=ebe7e7&lc=333333&sh=1&hfb=1&hbc=ECC9EE&hfc=FFFFFF"
           ></iframe>
         </Grid>
         <Grid item sm={12} md={6} style={classes.container}>
           <iframe
-            title="Playoff 2"
+            title="WCF"
             frameborder="0"
             scrolling="no"
             width="500"
             height="230"
-            src="https://statsdream.com/basketball/teams/l-a-lakers-367545/iframe/?type=team-next-match&sport=4&country=231&template=824&conference=Western Conference&team=367545&timezone=Pacific/Midway&time=24&width=500&height=230&font=Arial&fs=12&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&scfs=22&scfc=333333&scb=1&sclg=1&teamls=80&sh=1&hfb=1&hbc=ECC9EE&hfc=FFFFFF"
-          ></iframe>
-        </Grid>
-        <Grid item sm={12} md={6} style={classes.container}>
-          <iframe
-            title="Playoff 3"
-            frameborder="0"
-            scrolling="no"
-            width="500"
-            height="230"
-            src="https://statsdream.com/basketball/teams/denver-nuggets-367063/iframe/?type=team-next-match&sport=4&country=231&template=824&conference=Western Conference&team=367063&timezone=Pacific/Midway&time=24&width=500&height=230&font=Arial&fs=12&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&scfs=22&scfc=333333&scb=1&sclg=1&teamls=80&sh=1&hfb=1&hbc=ECC9EE&hfc=FFFFFF"
-          ></iframe>
-        </Grid>
-        <Grid item sm={12} md={6} style={classes.container}>
-          <iframe
-            title="Playoff 4"
-            frameborder="0"
-            scrolling="no"
-            width="500"
-            height="230"
-            src="https://statsdream.com/basketball/teams/miami-heat-367742/iframe/?type=team-next-match&sport=4&country=231&template=824&conference=Western Conference&team=367742&timezone=Pacific/Midway&time=24&width=500&height=230&font=Arial&fs=12&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&scfs=22&scfc=333333&scb=1&sclg=1&teamls=80&sh=1&hfb=1&hbc=ECC9EE&hfc=FFFFFF"
+            src="https://statsdream.com/basketball/teams/l-a-lakers-367545/iframe/?type=team-next-match&sport=4&country=231&template=824&conference=Eastern Conference&team=367545&timezone=Pacific/Midway&time=24&po=1&ma=0&wi=1&los=1&pts=0&ng=0&form=0&width=500&height=700&font=Arial&fs=12&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&ths=1&thb=1&thba=FFFFFF&thc=000000&bc=dddddd&hob=f5f5f5&hobc=ebe7e7&lc=333333&sh=1&hfb=1&hbc=ECC9EE&hfc=FFFFFF"
           ></iframe>
         </Grid>
       </Grid>
